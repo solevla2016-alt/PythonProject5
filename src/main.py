b3fd0aebb71dd371fa5833e420af7b40c9d9df32
Olga <solevla2016@gmail.com>
2026-02-17 21:17:38 +0300

    Проект дополнен модулем utils, в котором реализована фуекция чтения файла json. Внесены изменения в файл README

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
-import pytest
-from src.models import Product, Category
+from src.models import Category, Product
+from src.utils import load_data_from_json
 
 if __name__ == "__main__":
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product1 = Product(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+    )
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
@@ -21,9 +23,11 @@ if __name__ == "__main__":
     print(product3.price)
     print(product3.quantity)
 
-    category1 = Category("Смартфоны",
-                         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-                         [product1, product2, product3])
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
 
     print(category1.name == "Смартфоны")
     print(category1.description)
@@ -31,10 +35,12 @@ if __name__ == "__main__":
     print(category1.category_count)
     print(category1.product_count)
 
-    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-    category2 = Category("Телевизоры",
-                         "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-                         [product4])
+    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    category2 = Category(
+        "Телевизоры",
+        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        [product4],
+    )
 
     print(category2.name)
     print(category2.description)
@@ -43,3 +49,11 @@ if __name__ == "__main__":
 
     print(Category.category_count)
     print(Category.product_count)
+
+
+if __name__ == "__main__":
+    categories = load_data_from_json()
+    for cat in categories:
+        print(cat.name)
+        for prod in cat.products:
+            print(f"  {prod.name} — {prod.price} руб.")
